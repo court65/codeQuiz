c0cc32516140b173e54af51a4a49a3b2ca74dd5b
court65 <court_re@yahoo.com>
2022-04-11 13:49:07 -0500

    added the JS for question/answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,30 @@ var secondsLeft = 75;
 // BUTTONS
 const start = document.querySelector("#start");
 
+// START
 const codersIntro = document.querySelector("#challenge-begins");
 
+// CALL END
 const questionsE1 = document.querySelector(".all-question");
+
+//LOCATIONS
+let questionE1 = document.querySelector('#question')
+const correctWrong = document.querySelector("#right-wrong");
+let questionCount = 0;
+
+//FINAL SCORE
+const finalE1 = document.querySelector("#final-score");
+let initialInput = document.querySelector('#initials')
+
+
+// HIGH SCORE
+const highscoresE1 = document.querySelector("#high-scores");
+let scoreListE1 = document.querySelector('#score-list');
+let scoreList =[];
+
+
+
+const start = document.querySelector("#start");
 
 const start = document.querySelector("#start");
 
