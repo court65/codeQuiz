aec5be7e00c3431f57b021b39369fcc5b3fb8ebd
court65 <court_re@yahoo.com>
2022-04-11 13:19:05 -0500

    added JS for buttons

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// TIMER
+var time = document.querySelector(".timer");
+var score =document.querySelector("#score");
+var secondsLeft = 75;
+
+// BUTTONS
+const start = document.querySelector("#start");
+
+const codersIntro = document.querySelector("#challenge-begins");
+
+const questionsE1 = document.querySelector(".all-question");
+
+const start = document.querySelector("#start");
+
+const start = document.querySelector("#start");
