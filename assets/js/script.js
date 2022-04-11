caac1fe11e3494e8e13c361e17dc70e271f17a9a
court65 <court_re@yahoo.com>
2022-04-11 14:29:43 -0500

    added the questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,40 @@ let goBackBtn = document.querySelector("#goBack");
 
 //ANSWER CALL
 const ans1Btn = document.querySelector("#answer-1");
-
 const ans2Btn = document.querySelector("#answer-2");
 const ans3Btn = document.querySelector("#answer-3");
 const ans4Btn = document.querySelector("#answer-4");
+
+
+
+// Questions Array
+const questions = [
+    {
+        question: "Commonly used data types DO Not include:",
+        answers: ["1. strings", "2. booleans","3. alerts","4. numbers"],
+        correctAnswer: "2"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _______.",
+        answers: ["1. quotes", "2. curly brackets","3. parentheses","4. square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ______.",
+        answers: ["1. numbers and strings", "2. other arrays","3. booleans","4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: ["1. commas", "2. curly brackets","3. quotes","4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. terminal/bash","3. for loops","4. console.log"],
+        correctAnswer: "3"
+    }
+
+];
+
+
